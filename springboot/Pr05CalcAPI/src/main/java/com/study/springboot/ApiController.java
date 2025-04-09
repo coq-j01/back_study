4c3d547df5e894780155ef52a8ef24b966e75af8
coq-j01 <chaebinesxo@gmail.com>
2025-04-09 16:37:52 +0900

    pr05calcapi 수정

--- springboot/Pr05CalcAPI/src/main/java/com/study/springboot/ApiController.java
+++ springboot/Pr05CalcAPI/src/main/java/com/study/springboot/ApiController.java
@@ -1,6 +1,7 @@
 package com.study.springboot;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,28 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+    @Autowired
+    private ResDto resDto;
+
     @RequestMapping("/add")
     public ResDto add(@RequestBody ReqDto reqDto){
-        ResDto resDto = new ResDto();
         resDto.setResult(reqDto.getNum1() + reqDto.getNum2());
         return resDto;
     }
     @RequestMapping("/sub")
     public ResDto sub(@RequestBody ReqDto reqDto){
-        ResDto resDto = new ResDto();
         resDto.setResult(reqDto.getNum1() - reqDto.getNum2());
         return resDto;
     }
 
     @RequestMapping("/multi")
     public ResDto multi(@RequestBody ReqDto reqDto){
-        ResDto resDto = new ResDto();
         resDto.setResult(reqDto.getNum1() * reqDto.getNum2());
         return resDto;
     }
     @RequestMapping("/divi")
     public ResDto div(@RequestBody ReqDto reqDto){
-        ResDto resDto = new ResDto();
         resDto.setResult(reqDto.getNum1() / reqDto.getNum2());
         return resDto;
     }
--- springboot/Pr05CalcAPI/src/main/java/com/study/springboot/ResDto.java
+++ springboot/Pr05CalcAPI/src/main/java/com/study/springboot/ResDto.java
@@ -1,7 +1,9 @@
 package com.study.springboot;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
+@Component
 @Data
 public class ResDto {
     private int result;
