0c61553be8ac2b41e6f35833b0c1f0c9cd81ea3b
coq-j01 <chaebinesxo@gmail.com>
2025-04-14 09:23:35 +0900

    ex15JSP 파일 생성

---
+++ springboot/Ex15JSP/src/test/java/com/study/Ex15JSP/Ex15JspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Ex15JSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex15JspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
