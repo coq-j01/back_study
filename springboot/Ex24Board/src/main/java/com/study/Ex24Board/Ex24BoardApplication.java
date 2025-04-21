4ebd0c95272402953ed125a1e71525b62acc6706
coq-j01 <chaebinesxo@gmail.com>
2025-04-21 14:08:16 +0900

    ex24board 생성

---
+++ springboot/Ex24Board/src/main/java/com/study/Ex24Board/Ex24BoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.Ex24Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex24BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex24BoardApplication.class, args);
+	}
+
+}
---
+++ springboot/Ex24Board/src/test/java/com/study/Ex24Board/Ex24BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Ex24Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex24BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
